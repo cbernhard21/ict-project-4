31f0fd3cac98dbb649927b882b130d44df41aaf2
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2021-03-04 16:40:07 -0600

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -7,33 +7,53 @@ Display header and nav on all the html pages
 
 'use strict'
 
-export const mainNavLinks = [{
-    text: 'Home',
-    href: 'index.html'
-  },
-  {
-    text: 'Menu',
-    href: 'menu.html'
-  },
-  {
-    text: 'Contact',
-    href: 'contact.html'
-  },
-  {
-    text: 'Login',
-    href: 'login.html'
-  },
-  // {
-  //   text: 'Dashboard',
-  //   href: 'dashboard.html'
-  // }
-]
+// export const mainNavLinks = [
+//   {
+//     text: 'Home',
+//     href: 'index.html'
+//   },
+//   {
+//     text: 'Menu',
+//     href: 'menu.html'
+//   },
+//   {
+//     text: 'Contact',
+//     href: 'contact.html'
+//   },
+//   {
+//     text: 'Login',
+//     href: 'login.html'
+//   }
+// ]
 
-export function displayNavHtml(links) {
-  const navList = document.querySelector('#nav-list');
+// export function displayNavHtml(links) {
+//   const navList = document.querySelector('#nav-list');
 
-  links.forEach(link => {
-    let li = `<li class="nav-item"><a href="${link.href}">${link.text}</a></li>`
-    navList.innerHTML += li;
-  })
+//   links.forEach(link => {
+//     let li = `<li class="nav-item"><a href="${link.href}">${link.text}</a></li>`
+//     navList.innerHTML += li;
+//   })
+// }
+
+export function displayHeader() {
+  const header = document.querySelector('#header');
+  console.log('header function')
+  header.innerHTML = `
+  <div class="nav-wrapper container">
+    <p class="logo">Bistro <span class="logo-accent">715</span></p>
+    <nav class="nav-main">
+
+      <ul class="nav-list" id="nav-list">
+        <li class="nav-item"><a href="index.html">Home</a></li>
+        <li class="nav-item"><a href="menu.html">Menu</a></li>
+        <li class="nav-item"><a href="contact.html">Contact</a></li>
+        <li class="nav-item"><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
+    <div class="burger-menu">
+      <div class="burger-bar top-line"></div>
+      <div class="burger-bar middle-line"></div>
+      <div class="burger-bar bottom-line"></div>
+    </div>
+  </div>`;
 }
